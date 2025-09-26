0098f30ef586b1ad65e1d07e3e5ab470f25aae72
moonnyxjio <moonnyxjio@gmail.com>
2025-09-27 03:30:18 +0900

    .

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
+// src/App.js
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Learn from "./pages/Learn";   // ← 요걸로!
 import Exam from "./pages/Exam";
 import Records from "./pages/Records";
 import Badges from "./pages/Badges";
 import AdminLogin from "./pages/AdminLogin";
 
-function App() {
+export default function App() {
   return (
     <div className="app">
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/learn/:day" element={<Learn />} />
+        <Route path="/learn/:day" element={<Learn />} />  {/* ← Lesson 사용 */}
         <Route path="/exam/:day" element={<Exam />} />
         <Route path="/records" element={<Records />} />
         <Route path="/badges" element={<Badges />} />
@@ -19,5 +21,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
